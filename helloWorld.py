a7c7aec1f3fe0db0da29f293b0cad1e60bacc522
chala <chalajemal41@gmail.com>
2024-02-23 14:09:36 +0000

    adding a constant on our program

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,8 @@
+# Define a constant for the prompt message
+PROMPT_MESSAGE = "Enter something: "
+
 # Get input from the user
-user_input = input("Enter something: ")
+user_input = input(PROMPT_MESSAGE)
 
 # Print the inputted data
 print("You entered:", user_input)
