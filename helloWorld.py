e84e994a036f982f0cb934ab071c53d6e513ebf3
chala <chalajemal41@gmail.com>
2024-02-24 10:09:14 +0000

    Analyzing the inputted data and printing out the number of characters

--- helloWorld.py
+++ helloWorld.py
@@ -2,4 +2,18 @@
 user_input = input("Enter something: ")  # Prompt the user to enter some input and store it in the variable user_input
 
 # Print the inputted data
-print("You entered:", user_input)  # Print the inputted data along with a messagee
+print("You entered:", user_input)  # Print the inputted data along with a message
+
+# Count the number of characters
+num_characters = len(user_input)
+
+# Count the number of words
+num_words = len(user_input.split())
+
+# Count the number of lines
+num_lines = user_input.count('\n') + 1  # Count the newline characters to determine the number of lines
+
+# Print the analysis results
+print("Number of characters:", num_characters)
+print("Number of words:", num_words)
+print("Number of lines:", num_lines)
